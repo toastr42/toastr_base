4903a4f50efd70b46bc89caf0acc3f15e71139d9
Sean Darsie <sdarsie@Seans-iMac.local>
2017-10-23 20:55:15 -0700

    working router between home page and tweet page

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {render} from 'react-dom';
+import reactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom"
 
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
-import Tweet from './Tweet';
+import Main from './pages/Main'; // Our custom react component
+/* import Tweet from './pages/Tweet'; */
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(
-<Router history={hashHistory}>
+reactDOM.render( <BrowserRouter>
+    <Main/>
+</BrowserRouter>, document.getElementById('app'));
+
+/* <Router history={hashHistory}>
     <Route path="/" component={Main}>
         <IndexRoute component={Main}></IndexRoute>
         <Route path="tweet" name="tweet" component={Tweet}></Route>
     </Route>  
-</Router>, document.getElementById('app'));
+</Router> */
